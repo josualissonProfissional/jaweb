0a65e414feb548cfdbb5386045190e755eb6237e
Josualisson <josualisson17@gmail.com>
2025-01-14 16:21:43 -0300

    Versão 1.0

---
+++ assets/js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",   
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    keyboard: true,
+});
